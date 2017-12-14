3003780103515f6f4dbe5ac3f1d1a01c3971c2a0
bjorn terpstra <bjornterpstra@hotmail.com>
2017-12-14 12:18:59 +0100

    php website

---
+++ PHP/php website/website/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>php site</title>
+    <link rel="stylesheet" type="text/css" href="mystyle.css">
+</head>
+<body>
+<header>
+    <nav>
+        <ul>
+            <li><a href="main.php">main</a> </li>
+            <li><a href="form.php">form</a> </li>
+        </ul>
+    </nav>
+</header>
+<section>
---
+++ PHP/php website/website/main.php
@@ -0,0 +1,5 @@
+<?php
+    include("header.php");
+    echo 'Hello world!';
+
+    include ("footer.php");
