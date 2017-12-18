a3b58dddcd2f321bb5285a6dd2cfbbeaa6145259
bjorn terpstra <bjornterpstra@hotmail.com>
2017-12-18 13:27:27 +0100

    dynamische website

--- PHP/php website/website/form.php
+++ PHP/php website/website/form.php
@@ -1,6 +1,14 @@
 <?php
-include("header.php");
-echo 'Hello worm!';
+    include("header.php");
+?>
 
+    <form action="main.php" method="get">
+    Naam: <input type="text" name="roepnaam"><br>
+    favoriete kleur: <input type="text" name="favkleur">
+    <input type="submit">
+
+</form>
+
+<?php
 include ("footer.php");
-
+?>
--- PHP/php website/website/main.php
+++ PHP/php website/website/main.php
@@ -1,5 +1,34 @@
 <?php
     include("header.php");
-    echo 'Hello world!';
 
-    include ("footer.php");
+    $messages = array();
+
+   if(!isset($_GET["roepnaam"]) && !isset($_GET["favkleur"])) {
+        $messages[] = "Je moet eerst het formulier invullen";
+    } else if (strlen($_GET["roepnaam"]) < 2){
+        $messages[] = "een naam oet minimaal 2 letters bevatten";
+    }else if(strlen($_GET ["favkleur"]) < 3){
+        $messages[] = "Een kleur moet minimaal 3 letters bevatten";
+    }
+
+    if (!count($messages)== 0) {
+        //messages weergeven
+        $html = "<ul>";
+        foreach ($messages as $message) {
+            $html .= "<li>".$message."</li>";
+        }
+        $html .="</ul>";
+        echo $html;
+    }else{
+       echo "Hallo ". $_GET["roepnaam"]
+
+
+
+?>
+        <div style= "background-color:<?php echo $_GET["favkleur"]; ?>; height: 200px; width: 200px" > </div >
+
+<?php
+      }
+
+    include("footer.php");
+?>
